913dd22fd1569cc6c8e5e87d77ce30c285afcede
OmkarGajananMane <omkarm044@gmail.com>
2026-06-13 19:33:59 +0530

    Add files via upload

---
+++ C-Programming/Practice/program24.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    // Squence
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+    printf("Jay Ganesh...\n");
+
+    return 0;
+}
